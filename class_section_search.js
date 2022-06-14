f43551ca935bd845ce99fd845e927684d50e208e
nmautz <nmautz@zagmail.gonzaga.edu>
2022-06-14 10:44:24 -0700

    search func

---
+++ class_section_search.js
@@ -0,0 +1,53 @@
+
+class class_search_query{
+
+  constructor(subject, courseNumber){
+
+    this.subject = subject
+    this.courseNumber = courseNumber
+  }
+
+  checkClass(class_section){
+    if (class_section.subject != this.subject && this.subject != null)
+      return false
+    if (class_section.courseNumber != this.courseNumber && this.courseNumber != null)
+      return false
+    return true
+
+
+  }
+
+
+
+
+}
+
+
+function search_class(class_search_query)
+{
+  fetch("classes.json").then(response => response.json()).then( function(jsonData){
+
+    console.log("searching")
+    classesList = jsonData["data"]
+    for(var i = 0; i < classesList.length; ++i){
+      if(class_search_query.checkClass(classesList[i]))
+      {
+        console.log(classesList[i])
+      }
+
+
+
+    }
+
+
+
+  })
+  
+
+
+}
+
+
+var sec = new class_search_query("NURS", 403)
+
+search_class(sec)
