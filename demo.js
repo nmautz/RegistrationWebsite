0978b1650bb97e0365db99b93989eda989ca0e0a
Desktop <nathanmautz@outlook.com>
2022-06-13 17:21:43 -0700

    formatting

--- demo.js
+++ demo.js
@@ -1,126 +1,126 @@
 
 
-  /*Basic Functions*/
-  
+/*Basic Functions*/
 
 
-  // for search bar
 
-  var majorsArr = []
+// for search bar
 
-  window.onload=function(){
-    const input = document.getElementById("studyInput")
-    input.addEventListener('keyup',updateValue)
-  
-    function updateValue()
-    {   
-      var targetNum = 0  
-      const getInput = document.getElementById("studyInput").value
-      targetNum = setDropdown(getInput)
+var majorsArr = []
 
-      const elements1 = document.querySelectorAll(`[id^="a"]`)
-      console.log(targetNum)
-      for (var i = 0; i < elements1.length - targetNum - 1; ++i)
-      {
-        const element = document.getElementById("a")
-        element.remove()
-      }
-    }  
+window.onload=function(){
+  const input = document.getElementById("studyInput")
+  input.addEventListener('keyup',updateValue)
 
-    function setDropdown(fieldOfStudy)
+  function updateValue()
+  {   
+    var targetNum = 0  
+    const getInput = document.getElementById("studyInput").value
+    targetNum = setDropdown(getInput)
+
+    const elements1 = document.querySelectorAll(`[id^="a"]`)
+    console.log(targetNum)
+    for (var i = 0; i < elements1.length - targetNum - 1; ++i)
     {
-      var count = 0
-      for(var i = 0; i < majorsArr.length; ++i)
-      {
-        if (majorsArr[i].includes(fieldOfStudy))
-        {
-          addDropDown(majorsArr[i])
-          count++
-        } 
-
-      }
-      return count
+      const element = document.getElementById("a")
+      element.remove()
     }
-  
-    function addDropDown(fieldOfStudy)
-    {
-      const dropdownUI = document.getElementById("dropDown")
-      const aElement = document.createElement("a")
-      aElement.id = "a"
-      const text = document.createTextNode(fieldOfStudy)
-      aElement.appendChild(text)
-      dropdownUI.insertAdjacentElement("beforeend",aElement)
-    }
-  }
+  }  
 
-
-
-
-
-  function getClassById(id)
+  function setDropdown(fieldOfStudy)
   {
+    var count = 0
+    for(var i = 0; i < majorsArr.length; ++i)
+    {
+      if (majorsArr[i].includes(fieldOfStudy))
+      {
+        addDropDown(majorsArr[i])
+        count++
+      } 
 
-
-    for(i = 0; i < classesList.length; ++i){
-      if(classesList[i].id == id){
-        return classesList[i];
-      }
     }
-
+    return count
   }
+
+  function addDropDown(fieldOfStudy)
+  {
+    const dropdownUI = document.getElementById("dropDown")
+    const aElement = document.createElement("a")
+    aElement.id = "a"
+    const text = document.createTextNode(fieldOfStudy)
+    aElement.appendChild(text)
+    dropdownUI.insertAdjacentElement("beforeend",aElement)
+  }
+}
+
+
+
+
+
+function getClassById(id)
+{
+
+
+  for(i = 0; i < classesList.length; ++i){
+    if(classesList[i].id == id){
+      return classesList[i];
+    }
+  }
+
+}
   
-  function openClassInfoBox(id){
+function openClassInfoBox(id){
 
-    const classInfoDisplay = document.getElementById("class-info-display");
-    const classInfoDisplayName = document.getElementById("class-name-info-display") 
+  const classInfoDisplay = document.getElementById("class-info-display");
+  const classInfoDisplayName = document.getElementById("class-name-info-display") 
 
-    classInfoDisplay.setAttribute("style", "flex: 2;");
-    classInfoDisplay.style.opacity = "1";
+  classInfoDisplay.setAttribute("style", "flex: 2;");
+  classInfoDisplay.style.opacity = "1";
 
-    singleClass = getClassById(id);
+  singleClass = getClassById(id);
 
 
-    classInfoDisplayName.innerHTML = singleClass.courseTitle;
-  }
-  
-  function closeClassInfoBox(){
-    const classInfoDisplay = document.getElementById("class-info-display");
-    classInfoDisplay.setAttribute("style", "flex: 0;");
-    classInfoDisplay.style.opacity = "0";
-  }
+  classInfoDisplayName.innerHTML = singleClass.courseTitle;
+}
+
+function closeClassInfoBox(){
+  const classInfoDisplay = document.getElementById("class-info-display");
+  classInfoDisplay.setAttribute("style", "flex: 0;");
+  classInfoDisplay.style.opacity = "0";
+}
 
 
   //Class for creating template html elements like new class listings
-  class HTMLElementCreator {
+class HTMLElementCreator {
 
-    classListDisplay = null;
-  
-    constructor(){
-      this.classListDisplay = document.getElementById("class-list-display");
-    }
-    createClassListing() {
+  classListDisplay = null;
 
-      var singleClass = classesList[Math.floor(Math.random()*classesList.length)];
-
-      const divElement = document.createElement("div");
-      const textNode = document.createTextNode(singleClass.subject + singleClass.courseNumber);
-      divElement.classList.add("class-display")
-      divElement.appendChild(textNode);
-  
-      divElement.addEventListener("click", function(){
-        openClassInfoBox(singleClass.id);
-  
-  
-      })
-  
-  
-      this.classListDisplay.insertBefore(divElement, document.getElementById("add-class-button"))
-  
-    }
-  
-  
-  
+  constructor(){
+    this.classListDisplay = document.getElementById("class-list-display");
   }
+  createClassListing() {
+
+    var singleClass = classesList[Math.floor(Math.random()*classesList.length)];
+
+    const divElement = document.createElement("div");
+    const textNode = document.createTextNode(singleClass.subject + singleClass.courseNumber);
+    divElement.classList.add("class-display")
+    divElement.appendChild(textNode);
+
+    divElement.addEventListener("click", function(){
+      openClassInfoBox(singleClass.id);
+
+
+    })
+
+
+    this.classListDisplay.insertBefore(divElement, document.getElementById("add-class-button"))
+
+  }
+
+
+
+}
 
 
 
@@ -163,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function() {
   /*Get references for UI objects*/
   const closeClassInfoButton = document.getElementById("close-class-info-display-button");
   const addClassButton = document.getElementById("add-class-button")
-  
+
 
 
   closeClassInfoButton.addEventListener("click", function(){
