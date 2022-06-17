8a900999fbd587dec7b3181ed9137f9d2f6fd0c7
nmautz <nmautz@zagmail.gonzaga.edu>
2022-06-17 14:19:12 -0700

    forgotfilke

---
+++ saved_class_database.js
@@ -0,0 +1,34 @@
+function save_class(section){
+  localStorage.setItem(section.id, JSON.stringify(section))
+}
+
+function remove_class_by_ID(id){
+  localStorage.removeItem(id)
+}
+
+function load_classes(){
+  keys = Object.keys(localStorage)
+  values = []
+
+  for(var i = 0; i < keys.length; ++i)
+  {
+    values.push(JSON.parse(localStorage.getItem(keys[i])))
+
+
+  }
+  return values
+
+  
+}
+
+function is_class_saved(section){
+  classes = load_classes()
+  for(var i = 0; i < classes.length; ++i){
+    if(classes[i].id == section.id)
+    {
+      return true
+    }
+
+  }
+  return false
+}
