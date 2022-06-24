6062bd220dc3125b32ca2648cc2a602ac37e29eb
nmautz <nmautz@zagmail.gonzaga.edu>
2022-06-23 20:01:56 -0700

    implemented fix

--- saved_class_database.js
+++ saved_class_database.js
@@ -12,8 +12,14 @@ function load_classes(){
 
   for(var i = 0; i < keys.length; ++i)
   {
-    values.push(JSON.parse(localStorage.getItem(keys[i])))
+    //Item at key might not be a class
+    try{
+      values.push(JSON.parse(localStorage.getItem(keys[i])))
+    }catch{
+      //Delete unwanted key
+      localStorage.removeItem(keys[i])
 
+    }
 
   }
   return values
