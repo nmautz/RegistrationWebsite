87282feb2093873cf36c3899eb3f3bb612fc08c1
nmautz <nmautz@zagmail.gonzaga.edu>
2022-07-18 17:40:24 -0700

    s

---
+++ schedule.js
@@ -0,0 +1,15 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  create_schedule_elements(document.getElementById("content-main"), "one")
+
+})
+
+
+function create_schedule_elements(parent, planID){
+
+  console.log(planID)
+
+
+
+}
