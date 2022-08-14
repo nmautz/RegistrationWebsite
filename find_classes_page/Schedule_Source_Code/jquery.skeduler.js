7aae7faee33129c9a741fa0cbe32f2683cd2fcb2
nmautz <nmautz@zagmail.gonzaga.edu>
2022-08-14 13:17:21 -0700

    basic resizability with a few bugs

--- find_classes_page/Schedule_Source_Code/jquery.skeduler.js
+++ find_classes_page/Schedule_Source_Code/jquery.skeduler.js
@@ -170,6 +170,7 @@
   * - borderWidth - width of border of cell in grid
   */
   $.fn.skeduler = function (options) {
+    $( "#plan_container" ).resizable({handles:'n'});
     settings = $.extend(defaultSettings, options);
 
     if (settings.debug) {
