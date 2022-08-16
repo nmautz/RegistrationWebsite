72f36790ac8252827f819d4433913e9fde0a192e
benberton <91513576+benberton@users.noreply.github.com>
2022-08-15 21:44:15 -0700

    untill enrolment added

--- find_classes_page/Pop_Up/Pop_Up.js
+++ find_classes_page/Pop_Up/Pop_Up.js
@@ -3,7 +3,17 @@ function open_pop_up(section)
     const popUp = document.getElementById("pop_up_window")
     const overlay = document.getElementById("overlay")
     // console.log(sectionID.courseTitle)
-    document.getElementById("popUp-title").innerHTML = section.courseTitle
+    document.getElementById("popUp-title").innerHTML = section.courseTitle + "<span style='font-weight:normal'> | </span>" + section.subject + section.courseNumber + "<span style='font-weight:normal'> | </span>" + section.creditHourSession + " credit hours"
+    document.getElementById("popUp-Description").innerHTML = section.description
+    document.getElementById("popUp-Professor").innerHTML = section.professorName + " | " + section.professorEmail
+    var meetingTimes = parse_time(section.beginTime) + "-" + parse_time(section.endTime)
+    document.getElementById("popUp-Meeting-Times").innerHTML = meetingTimes
+    var meetingDays = requirement.getWeekString(section)
+    document.getElementById("popUp-Meeting-Days").innerHTML = meetingDays
+    document.getElementById("popUp-Location").innerHTML = section.campusDescription + " Campus | " + section.buildingDescription
+    document.getElementById("popUp-Enrolment").innerHTML = section.enrollment + "/" + section.maximumEnrollment
+    
+    
     if (popUp == null)
         return
     popUp.classList.add("active")
