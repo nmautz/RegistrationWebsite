9e5fa3c9f0fd599bb84193617bcf328ba49f4c51
benberton <91513576+benberton@users.noreply.github.com>
2022-08-22 23:24:34 -0700

    campus restriction added to main search dropdown

--- find_classes_page/DropDown/MainSearch.js
+++ find_classes_page/DropDown/MainSearch.js
@@ -33,35 +33,39 @@ class mainSearch extends dropDown{
 
         for(var i = 0; i < classesList.length; ++i)
         {
-            var elements = [classesList[i].subject + " " + classesList[i].courseNumber + " - " + classesList[i].courseTitle,classesList[i].professorName,classesList[i].attributeDesc,classesList[i].subjectDescription]
-            for (var j = 0; j < elements.length; ++j)
+            if(requirement.meetsCampusReq(classesList[i]))
             {
-                
-                var text = String(elements[j]).toUpperCase()
-                if(elementNames[j] == "Attributes")
+                var elements = [classesList[i].subject + " " + classesList[i].courseNumber + " - " + classesList[i].courseTitle,classesList[i].professorName,classesList[i].attributeDesc,classesList[i].subjectDescription]
+                for (var j = 0; j < elements.length; ++j)
                 {
-                    var textArr = String(elements[j])
-                    // console.log(elementContainer[i][j])
-                    var tempArr = textArr.split(',')
-                    for (var x = 0; x < tempArr.length; ++x)
-                    {   
-                        var indexVal = String(tempArr[x]).toUpperCase()
-                        if (indexVal.includes(userInput))
-                        {
-                            if (!searchElements[j].container.includes(tempArr[x]))
-                                searchElements[j].container.push(tempArr[x]) 
-                        }     
-                    }
-                        
-                }else if (text.includes(userInput))
-                {
-                    var checkVal = String(searchElements[j].container).toUpperCase()
-                    if (!checkVal.includes(text))
-                        searchElements[j].container.push(elements[j])
                     
+                    var text = String(elements[j]).toUpperCase()
+                    if(elementNames[j] == "Attributes")
+                    {
+                        var textArr = String(elements[j])
+                        // console.log(elementContainer[i][j])
+                        var tempArr = textArr.split(',')
+                        for (var x = 0; x < tempArr.length; ++x)
+                        {   
+                            var indexVal = String(tempArr[x]).toUpperCase()
+                            if (indexVal.includes(userInput))
+                            {
+                                if (!searchElements[j].container.includes(tempArr[x]))
+                                    searchElements[j].container.push(tempArr[x]) 
+                            }     
+                        }
+                            
+                    }else if (text.includes(userInput))
+                    {
+                        var checkVal = String(searchElements[j].container).toUpperCase()
+                        if (!checkVal.includes(text))
+                            searchElements[j].container.push(elements[j])
                         
-                }
-            }      
+                            
+                    }
+                }       
+            }
+           
         }
         
         for (var i = 0; i < searchElements.length; ++i)
