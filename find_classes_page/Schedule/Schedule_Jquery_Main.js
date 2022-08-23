c223669a2777e38d6691c1951c1102a8350a082b
nmautz <nmautz@zagmail.gonzaga.edu>
2022-08-23 12:56:35 -0700

    bugfix

--- find_classes_page/Schedule/Schedule_Jquery_Main.js
+++ find_classes_page/Schedule/Schedule_Jquery_Main.js
@@ -7,15 +7,11 @@
         headers: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
         tasks: tasks,
         cardTemplate: '<div>${id}</div><div>${title}</div>',
-<<<<<<< HEAD
         onClick: function (e, t)
         {
             var section = get_class(schedule.selectedPlan,t.id)
             open_pop_up(section)
         }
-=======
-        onClick: function (e, t) { console.log(e, t); },
->>>>>>> resizable_window
     });
     }
 
