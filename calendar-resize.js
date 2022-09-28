45fccae1eb1f613a41dce33f4d8a11c809ee8520
nmautz <nmautz@zagmail.gonzaga.edu>
2022-09-28 16:59:45 -0700

    expand collapse func

--- calendar-resize.js
+++ calendar-resize.js
@@ -2,7 +2,26 @@
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-  const cal_display = document.getElementById("calendar-display");
+  const cal = document.getElementById("calendar-container");
+  const handle = document.getElementById("resize-handle");
+  var open = false;
+
+  handle.addEventListener("click", ()=>{
+
+
+    if(open){
+      cal.style.height = "20px";
+      handle.innerHTML = 'Expand'
+      open=false
+    }else{
+      cal.style.height = "80%";
+      handle.innerHTML="Collapse";
+      open = true;
+
+    }
+
+
+  })
 
 
 
