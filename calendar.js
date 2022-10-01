98174bb9764a4e3d9f24562579633c4be7cc35f3
nmautz <nmautz@zagmail.gonzaga.edu>
2022-09-30 18:29:14 -0700

    propertimes

--- calendar.js
+++ calendar.js
@@ -45,14 +45,55 @@ class Calendar{
     var time_td = document.createElement("td");
     row.appendChild(time_td);
 
-    time_td.innerHTML = time;
+    time_td.innerHTML = min_to_str_time(time);
     time_td.classList.add = "calendar-time";
-
-
-
   }
 
 
 }
 
+
+function min_to_str_time(min){
+
+
+  var hour = Math.floor(min/60);
+  var ampm = "";
+
+  if(hour > 12){
+    hour = hour - 12;
+    ampm = "PM"
+
+  }else if(hour == 12){
+
+    ampm = "PM"
+
+    
+  }else{
+
+    ampm = "AM"
+
+
+  }
+
+
+  var hour_str = hour.toString();
+  var min_str = (min % 60).toString();
+
+
+
+  if(min_str.length == 1){
+    min_str = "0"+min_str;
+  }
+  if(hour_str.length == 1){
+    hour_str = "0"+hour_str;
+  }
+
+
+
+
+
+  return (hour_str +":" + min_str + " " + ampm)
+
+
+}
 
